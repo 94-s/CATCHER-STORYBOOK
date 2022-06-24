7db637bfa7c7251b211ff616a4fc1a26a1b61abd
slobbie <haeru9410@gmail.com>
2022-06-24 23:51:12 +0900

    :package: Styled-component 추가 / button ui 구현 시작

--- src/stories/Button.tsx
+++ src/stories/Button.tsx
@@ -1,9 +1,51 @@
 import React from 'react';
+import './button.css';
+import styled, { css } from 'styled-components';
 
-export interface ButtonProps {}
-
-function Button({ ...props }: ButtonProps) {
-  return <button>Hello</button>;
+export interface ButtonProps {
+  /**
+   * Button label
+   */
+  label: string;
+  /**
+   * Button color
+   */
+  color: string;
+  /**
+   * Button disable는 valid가 통과하지 않을시 false
+   */
+  disabled?: boolean;
+  /**
+   * Button Valid는 사용하지 않을시 true표기
+   */
+  Valid?: string;
 }
 
+const Button = ({ ...props }: ButtonProps) => {
+  return (
+    <button
+      className={`Button-style ${props.Valid ? '' : 'Button-disabled'}`}
+      style={{ backgroundColor: props.color }}
+      disabled={!props.Valid}
+      {...props}
+    >
+      {props.label}
+    </button>
+  );
+};
+
 export default Button;
+
+const ButtonModel = styled.button<ButtonProps>`
+  color: white;
+  background-color: '#000';
+  cursor: pointer;
+  border: none;
+  border-radius: 16px;
+  margin: 5px;
+  opacity: 0.7;
+
+  &:hover {
+    background-color: '#000';
+  }
+`;
---
+++ src/themes/color.ts
@@ -0,0 +1,12 @@
+const white = '#ffffff';
+const black = '#000000';
+const mainColor = '#FC762A';
+
+const Colors = {
+  white,
+  black,
+  mainColor,
+  Gray300: '#EFEFEF',
+};
+
+export default Colors;
