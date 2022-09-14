97aca30d0209dd7d38b932ef6f44f660ff157b8f
jiyoung <bjyoung0@gmail.com>
2022-09-14 21:08:45 +0900

    [feat] SearchInput Component 분리

---
+++ src/components/SearchInput/SearchInput.stories.tsx
@@ -0,0 +1,43 @@
+/**@jsxImportSource @emotion/react */
+import { Meta } from "@storybook/react";
+import { SearchInput, SearchInputProps } from "./SearchInput";
+
+export default {
+  title: "components/SearchInput",
+  component: SearchInput,
+  arg: {},
+} as Meta<SearchInputProps>;
+
+export const ProductSearch = (args: SearchInputProps) => {
+  const search = (keyword: string) => {
+    console.log("search: ", keyword);
+  };
+  const autoComplete = (keyword: string) => {
+    console.log("autoComplete: ", keyword);
+  };
+
+  return (
+    <SearchInput
+      placeholder="찾는 상품이 있으신가요?"
+      search={search}
+      autoComplete={autoComplete}
+    />
+  );
+};
+
+export const BrandSearch = (args: SearchInputProps) => {
+  const search = (keyword: string) => {
+    console.log("search: ", keyword);
+  };
+  const autoComplete = (keyword: string) => {
+    console.log("autoComplete: ", keyword);
+  };
+
+  return (
+    <SearchInput
+      placeholder="찾는 브랜드가 있으신가요?"
+      search={search}
+      autoComplete={autoComplete}
+    />
+  );
+};
