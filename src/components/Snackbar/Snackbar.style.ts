9e33d615e98e970b291be810e757ee189b65a25c
beaever <thebeaever@gmail.com>
2022-09-20 00:33:31 +0900

    [feat] Snackbar 기본 세팅

---
+++ src/components/Snackbar/Snackbar.style.ts
@@ -0,0 +1,6 @@
+import { css } from '@emotion/react';
+import { Colors, Fonts } from '../../general/styles/themes';
+
+const snackbarContainerStyle = css``;
+
+export { snackbarContainerStyle };
---
+++ src/components/Snackbar/Snackbar.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { snackbarContainerStyle } from './Snackbar.style';
+
+export interface SnackbarProps {}
+
+const Snackbar = () => {
+  return <></>;
+};
+
+export default Snackbar;
