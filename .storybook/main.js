62b818c125f7b459330a7915d8ac485ebfbd0a74
beaever <thebeaever@gmail.com>
2022-09-19 22:51:44 +0900

    [feat] path general 추가 설정

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,36 +1,37 @@
-const path = require("path");
+const path = require('path');
 
 const toPath = (_path) => path.join(process.cwd(), _path);
 
 module.exports = {
-  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    "@storybook/preset-create-react-app",
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    '@storybook/preset-create-react-app',
   ],
   webpackFinal: async (config) => ({
     ...config,
     resolve: {
       ...config.resolve,
-      extensions: [".js", ".jsx", ".ts", ".tsx"],
+      extensions: ['.js', '.jsx', '.ts', '.tsx'],
       alias: {
         ...config.resolve.alias,
-        "@emotion/core": toPath("node_modules/@emotion/react"),
-        "@src": path.resolve(__dirname, "../src"),
-        "@components": path.resolve(__dirname, "../src/components"),
-        "@hooks": path.resolve(__dirname, "../src/hooks"),
+        '@emotion/core': toPath('node_modules/@emotion/react'),
+        '@src': path.resolve(__dirname, '../src'),
+        '@components': path.resolve(__dirname, '../src/components'),
+        '@general': path.resolve(__dirname, '../src/general'),
+        '@Hooks': path.resolve(__dirname, '../src/hooks'),
       },
     },
   }),
 
-  framework: "@storybook/react",
+  framework: '@storybook/react',
   core: {
-    builder: "@storybook/builder-webpack5",
+    builder: '@storybook/builder-webpack5',
   },
   typescript: {
-    reactDocgen: "react-docgen-typescript",
+    reactDocgen: 'react-docgen-typescript',
     reactDocgenTypescriptOptions: {
       compilerOptions: {
         allowSyntheticDefaultImports: false,
