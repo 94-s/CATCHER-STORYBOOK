20d1a8d73b8ecd8669dfcd290f5ad36a2802c951
Jinyoung Kim <76705423+beaever@users.noreply.github.com>
2022-09-19 22:26:15 +0900

    [개발] svg export 추가 설정 (#18)

--- src/components/Icon/Icon.stories.tsx
+++ src/components/Icon/Icon.stories.tsx
@@ -1,6 +1,13 @@
 /**@jsxImportSource @emotion/react */
 import { Meta } from '@storybook/react';
+import { css } from '@emotion/react';
 import { Icon, IconProps } from './Icon';
+import { Colors } from '../styles/themes';
+
+const layout = css`
+  display: flex;
+  background-color: ${Colors.palette.gray30};
+`;
 
 export default {
   title: 'components/Icon',
@@ -9,5 +16,20 @@ export default {
 } as Meta<IconProps>;
 
 export const Base = (args: IconProps) => {
-  return <Icon icon='icAlert' />;
+  return (
+    <div css={layout}>
+      <Icon icon='icAlert' />
+      <Icon icon='icCamera' />
+      <Icon icon='icChecked' />
+      <Icon icon='icCodeBrackets' />
+      <Icon icon='icColors' />
+      <Icon icon='icComments' />
+      <Icon icon='icDirection' />
+      <Icon icon='icFlow' />
+      <Icon icon='icHeaderLogo' />
+      <Icon icon='icInputDelete' />
+      <Icon icon='icInputModify' />
+      <Icon icon='icNotionLogo' />
+    </div>
+  );
 };
