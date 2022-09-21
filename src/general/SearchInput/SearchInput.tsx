3fd32bed4be69409d60e328395b4f0e7c7792414
jiyoung <bjyoung0@gmail.com>
2022-09-21 17:39:12 +0900

    [refactor] 코드 스타일 수정
    - 컴포넌트 형식 다른 컴포넌트와 통일
    - 주석 작성

--- src/general/SearchInput/SearchInput.tsx
+++ src/general/SearchInput/SearchInput.tsx
@@ -1,14 +1,14 @@
 /**@jsxImportSource @emotion/react */
-import React from 'react';
-import {
-  containerStyle,
-  searchInputStyle,
-  buttonStyle,
-} from './SeachInput.style';
+import * as React from 'react';
+import * as styles from './SeachInput.style';
 import { Icon } from '@src/general/Icon';
 import useChange from '@hooks/useChange';
 import useDisplay from '@hooks/useDisplay';
 
+/**
+ * search: 검색 시 동작할 함수
+ * autoComplete: 자동완성 시 동작할 함수
+ */
 export interface SearchInputProps {
   className?: string;
   placeholder?: string;
@@ -43,10 +43,14 @@ export const SearchInput = ({
 
   return (
     <div>
-      <form css={containerStyle()} method="GET" onSubmit={handelFormSubmit}>
+      <form
+        css={styles.containerStyle()}
+        method="GET"
+        onSubmit={handelFormSubmit}
+      >
         <input
           type="search"
-          css={searchInputStyle()}
+          css={styles.searchInputStyle()}
           className={className}
           value={value}
           onChange={onChange}
@@ -55,7 +59,7 @@ export const SearchInput = ({
           onFocus={openDisplay}
           onBlur={closeDisplay}
         />
-        <button type="submit" css={buttonStyle()}>
+        <button type="submit" css={styles.buttonStyle()}>
           <Icon
             icon="icSearch"
             width="15px"
@@ -73,3 +77,5 @@ export const SearchInput = ({
     </div>
   );
 };
+
+export default SearchInput;
