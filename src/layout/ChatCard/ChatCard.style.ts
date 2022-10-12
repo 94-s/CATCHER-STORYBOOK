228b9befc822c5fb4b8b518fd964d37d632c5557
16010948 <bjyoung0@gmail.com>
2022-10-12 17:43:44 +0900

    [feat] ChatCard 구현

---
+++ src/layout/ChatCard/ChatCard.style.ts
@@ -0,0 +1,61 @@
+import { css } from "@emotion/react";
+import { Colors } from "@styles/themes";
+
+const containerStyle = () => css`
+  display: flex;
+  width: 100%;
+`;
+
+const chatCardMode = (me: boolean) =>
+  me
+    ? `
+      &,
+      &::before {
+        background-color: ${Colors.element.primary500};
+        color: ${Colors.palette.gray10};
+      }
+
+      &::before {
+        top: 50%;
+        left: -0.25rem;
+        transform: translate(0, -50%) rotate(45deg);
+      }
+    `
+    : `
+    &,
+    &::before {
+      background-color: ${Colors.palette.gray50};
+      color: ${Colors.palette.gray80};
+    }
+
+    &::before {
+      top: 50%;
+      right: -0.25rem;
+      transform: translate(0, -50%) rotate(45deg);
+    }
+    `;
+
+const chatCardStyle = (me: boolean) => css`
+  position: relative;
+  display: inline-block;
+  width: max-content;
+  height: max-content;
+  max-width: 10rem;
+  padding: 0.5rem 0.7rem;
+  border-radius: 0.3rem;
+  white-space: pre;
+
+  &::before {
+    position: absolute;
+  }
+
+  &::before {
+    content: "";
+    width: 0.5rem;
+    height: 0.5rem;
+  }
+
+  ${chatCardMode(me)}
+`;
+
+export { containerStyle, chatCardStyle };
---
+++ src/layout/ChatCard/ChatCard.tsx
@@ -0,0 +1,30 @@
+/**@jsxImportSource @emotion/react */
+import React from 'react';
+import { containerStyle, chatCardStyle } from './ChatCard.style';
+
+/**
+ * message: 채팅 메시지
+ * me: 내가 한 말인지 상대방이 한 말인지
+ */
+export interface ChatCardProps {
+  className?: string;
+  message: string;
+  me: boolean;
+}
+
+export const ChatCard = ({
+  className,
+  message,
+  me
+}: ChatCardProps) => {
+  return (
+    <div css={containerStyle()}>
+      <div  
+        css={chatCardStyle(me)}>
+          {message}
+      </div>
+    </div>
+  );
+};
+
+export default ChatCard;
