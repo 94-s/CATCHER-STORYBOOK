303b5abfe9a6455f38a91149cd20e05b5885dee0
beaever <thebeaever@gmail.com>
2022-10-29 17:24:47 +0900

    [feature] Snackbar stories.tsx 파일 수정

--- src/components/Snackbar/Snackbar.stories.tsx
+++ src/components/Snackbar/Snackbar.stories.tsx
@@ -5,11 +5,27 @@ import Snackbar, { SnackbarProps } from './Snackbar';
 export default {
   title: 'components/Snackbar',
   component: Snackbar,
-  argTypes: {},
+  argTypes: {
+    message: {
+      description: 'snackbar안에 들어갈 messge 를 입력',
+      defaultValue: '메세지를 입력하세요!',
+      type: 'string',
+    },
+    stateType: {
+      description: 'snackbar의 state type 에 따른 스타일 변경',
+      defaultValue: 'success',
+      options: ['success', 'danger', 'error'],
+      control: { type: 'select' },
+    },
+  },
 } as Meta<SnackbarProps>;
 
 export const Base = (args: SnackbarProps) => {
   return (
     <Snackbar {...args} message='메세지를 입력하세요!' stateType='success' />
   );
+};
+
+export const StateType = (args: SnackbarProps) => {
+  return <Snackbar {...args} message='메세지를 입력하세요!' />;
 };
